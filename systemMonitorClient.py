d2076c8ef6ef98e2d1a60f2e8fd6b6ee5f2fcd0f
Mike <michaelpulliam1@gmail.com>
2016-06-14 14:25:25 -0600

    Make things threaded!
    Allow the RPi to talk back to the 'client', cached LCD changes, code cleanup, etc.
    
    I think I may need to create a configuration file at some point so the python doesn't need to be editited for each network.

--- systemMonitorClient.py
+++ systemMonitorClient.py
@@ -3,9 +3,11 @@ import time
 import socket
 import psutil
 import pickle
+import threading
 
 # Vars
 host = '192.168.11.200'
+client = ''
 port = 7166
 red = (1, 0, 0)
 green = (0, 1, 0)
@@ -15,13 +17,18 @@ magenta = (1, 0, 1)
 cyan = (0, 1, 1)
 white = (1, 1, 1)
 off = (0, 0, 0)
-disk1 = 'D:\\'
-disk2 = 'C:\\'
-
-colors = {red, green, blue}
+disk1 = 'C:\\'
+disk2 = 'D:\\'
+disk3 = 'E:\\'
+state = 'U'
+threadRun = 1
+clear = 1
+spaces = ('clear', 'doot')
+onOff = 1
 # init doodads
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((host, port))
+#sock.connect((host, port))
+sock.bind((client, port))
 # Functions
 # Converts bytes to human readable values, Thanks Stackoverflow!
 def humanSize(num):
@@ -32,31 +39,75 @@ def humanSize(num):
 		rounded_val = round(float(num) / 2 ** exp_str[i][0], 2)
 	return '%s%s' % (int(rounded_val), exp_str[i][1])
 
+# Listens for button presses and changes a variable
+def listenForButtons():
+	while threadRun:
+		header, data = pickle.loads(sock.recv(256))
+		# Debugging
+		#print(header)
+		if header == 'button':
+			global state
+			state = data
+			global clear
+			clear = 1
+		if header == 'select':
+			global onOff
+			if onOff:
+				onOff = 0
+			else:
+				onOff = 1
+
+t = threading.Thread(target=listenForButtons, args=())
+try:
+	t.start()
+except:
+	print("Failed to start listener thread... Just CPU usage sent.")
 try:
 	while 1:
-		# CPU Usage data
-		cpuUsage = psutil.cpu_percent(interval = 1)
-		data = 'CPU: ' + repr(int(cpuUsage)) + '%'
-		while len(data) < 9:
-			data += ' '
+		if state == 'U':
+			# Cleanup the display if needed
+			if clear:
+				sock.sendto(pickle.dumps(spaces, protocol = 2), (host, port))
+				clear = 0
+			# CPU Usage data
+			cpuUsage = psutil.cpu_percent(interval = 1)
+			data = 'CPU: ' + repr(int(cpuUsage)) + '%'
+			while len(data) < 9:
+				data += ' '
 
-		# Disk Usage data
-		total1, used1, free1, percent1 = psutil.disk_usage(disk1)
-		total2, used2, free2, percent2 = psutil.disk_usage(disk2)
-		data += '\nC:/ ' + humanSize(used2) + '/' + humanSize(total2)
+			# RAM usage data
+			mem = psutil.virtual_memory()
+			data += '\nRAM: ' + humanSize(mem.used) + '/' + humanSize(mem.total)
 
-		if cpuUsage <= 25:
-			color = cyan
-		elif cpuUsage > 25 and cpuUsage <= 50:
-			color = green
-		elif cpuUsage <= 75 and cpuUsage > 50:
-			color = yellow
-		elif cpuUsage > 75:
-			color = red
+			if cpuUsage <= 25:
+				color = cyan
+			elif cpuUsage > 25 and cpuUsage <= 50:
+				color = green
+			elif cpuUsage <= 75 and cpuUsage > 50:
+				color = yellow
+			elif cpuUsage > 75:
+				color = red
 
-		sdata = ('message', data)
-		scolor = ('color', color)
+			sdata = ('message', data)
+			scolor = ('color', color)
 
+		if state == 'R':
+			if clear:
+				sock.sendto(pickle.dumps(spaces, protocol = 2), (host, port))
+				clear = 0
+			# Disk Usage data
+			time.sleep(5)
+			total1, used1, free1, percent1 = psutil.disk_usage(disk1)
+			total2, used2, free2, percent2 = psutil.disk_usage(disk2)
+			total3, used3, free3, percent3 = psutil.disk_usage(disk3)
+
+			data = 'C:/ ' + humanSize(used1) + '/' + humanSize(total1)
+			data +='\nE:/ ' + humanSize(used3) + '/' + humanSize(total3)
+
+			scolor = ('color', cyan)
+			sdata = ('message', data)
+		if not onOff:
+			scolor = ('color', off)
 		sock.sendto(pickle.dumps(sdata, protocol = 2), (host, port))
 		sock.sendto(pickle.dumps(scolor, protocol = 2), (host, port))
 		time.sleep(0.1)
@@ -64,4 +115,5 @@ except KeyboardInterrupt:
 	print("Ctrl-C Recived, Stopping!")
 
 finally:
+	threadRun = 0
 	sock.close()
--- systemMonitorServer.py
+++ systemMonitorServer.py
@@ -4,18 +4,50 @@ import time
 import Adafruit_CharLCD as LCD
 import socket
 import pickle
+import threading
 
 # Variables
 host = ''
 port = 7166
+client = '192.168.11.202'
+threadRun = 1
+oldData = 'abcdefghijklmnopqrstuvwxyzABCDEF'
+column = 0
+row = 0
+loc = 0
 
 # init doodads
 lcd = LCD.Adafruit_CharLCDPlate()
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+buttons = ((LCD.SELECT, 'K'), (LCD.LEFT, 'L'),
+ (LCD.UP, 'U'), (LCD.RIGHT, 'R'), (LCD.DOWN, 'D'))
 
 sock.bind((host, port))
 # Listen for a client wanting to send LCD data
+
+# Functions
+# Poll the buttons and send a packet if one is pressed
+def pollReply():
+	while threadRun:
+		for button in buttons:
+			if lcd.is_pressed(button[0]):
+				if button[0] is not LCD.SELECT:
+					sData = ('button', button[1])
+					sock.sendto(pickle.dumps(sData, protocol = 2), (client, port))
+					time.sleep(0.15)
+				else:
+					sData = ('select', button[1])
+					time.sleep(0.3)
+					sock.sendto(pickle.dumps(sData, protocol = 2), (client, port))
+					time.sleep(0.3)
+		time.sleep(0.15)
+
+t = threading.Thread(target=pollReply, args=())
+try:
+	t.start()
+except:
+	print("Failed to start button poller, Won't send button presses!")
+
 print("Press Ctrl-C to stop the server")
 
 try:
@@ -29,22 +61,54 @@ try:
 
 		else:
 			if data:
-				# The problem
 				if header == 'message':
-					lcd.clear()
-					lcd.message(data)
-					print("Successfully wrote " + data + " to the display!")
+					for char in data:
+						if char == '\n':
+							column = 0
+							row = 1
+						elif oldData[loc] == char:
+							if column >= 15:
+								column = 0
+								row = 1
+								column += 1
+								loc += 1
+							else:
+								column +=1
+								loc += 1
+						else:
+							lcd.set_cursor(column, row)
+							lcd.message(char)
+							if column >= 15:
+								column = 0
+								row = 1
+								column += 1
+								loc += 1
+							else:
+								column += 1
+								loc += 1
+					oldData = data
+					while len(oldData) < 32:
+						oldData += ' '
+					column = 0
+					row = 0
+					loc = 0
+					# Debugging
+					#print("Successfully wrote " + data + " to the display!")
 				if header == 'color':
 					red, green, blue = data
 					lcd.set_color(red, green, blue)
-			else:
-				break
+				if header == 'clear':
+					lcd.clear()
+					oldData = ''
+					while len(oldData) < 32:
+						oldData += ' '
 
 except KeyboardInterrupt:
 	# Handle Ctrl-C
 	print("Ctrl-C Recived, Stopping!")
 finally:
 	# Cleanup
+	threadRun = 0
 	sock.close()
 	lcd.clear()
 	lcd.set_color(0, 0, 0)
